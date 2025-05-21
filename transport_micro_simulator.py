6c6914a20f16d35ed02035c20be64a8337380c1a
Chris D <cxd309@student.bham.ac.uk>
2025-05-21 15:51:49 +0100

    Moved to path based logic

--- transport_micro_simulator.py
+++ transport_micro_simulator.py
@@ -1,51 +1,32 @@
 from dataclasses import dataclass
+import networkx as nx
 from typing import List
-
-class Vehicle:
-  def __init__(self, v_max: float, a_acc: float, a_dcc: float):
-    self.v_max = v_max  # maximum line speed (m/s)
-    self.a_acc = a_acc  # acceleration (m/s^2)
-    self.a_dcc = a_dcc  # deceleration (m/s^2)
-
-    self.s_acc = (self.v_max ** 2) / (2 * self.a_acc)  # distance to accelerate to v_max (m)
-    self.s_dcc = (self.v_max ** 2) / (2 * self.a_dcc)  # distance to stop from v_max (m)
-
-    self.t_acc = self.v_max / self.a_acc  # time to accelerate to v_max (s)
-    self.t_dcc = self.v_max / self.a_dcc  # time to stop from v_max (s)
-
-class Operations:
-  def __init__(self, t_hw: float, t_dw: float):
-    self.t_hw = t_hw  # headway (s)
-    self.t_dw = t_dw  # dwell time (s)
-
-class Journey:
-  def __init__(self, s_j: float, v_walk: float):
-    self.s_j = s_j  # journey distance (m)
-    self.v_walk = v_walk  # walking speed (m/s)
+import json
 
 @dataclass
-class SimulationResult:
-    n_stn: int          # number of stations
-    s_is: float         # interstation distance (m)
-    t_vehicle: float    # vehicle time (s)
-    t_access: float     # access time (s)
-    t_wait: float       # wait time (s)
-    t_dtd: float        # door-to-door journey time (s)
+class Location:
+  x: float
+  y: float
 
 @dataclass
-class FormValues:
-    s_j: float       # journey distance (km)
-    tphpd: float     # trains per hour per direction
-    v_max: float     # maximum line speed (km/h)
-    a_acc: float     # mean acceleration (m/s^2)
-    a_dcc: float     # mean deceleration (m/s^2)
-    t_dw: float      # dwell time (mins)
-    v_walk: float    # average walking pace (km/h)
+class Path:
+  orig: str # id for origin of path
+  dest: str # id for destination of path
+  len: float # length of the path
 
-@dataclass
-class SimulationParameters:
-    veh: Vehicle      # vehicle parameters
-    ops: Operations   # operations parameters
-    jny: Journey      # journey parameter
+class Infrastructure:
+  def __init__(self, pathList: List[Path]):
+    self.pathList = pathList
 
-function 
+    self.G = self.build_graph()
+
+  def build_graph(self) -> nx.DiGraph:
+    G = nx.DiGraph()
+    for path in self.pathList:
+      G.add_edge(path.orig, path.dest, weight=path.len)
+    return G
+
+def load_paths(path_file):
+  with open(path_file) as file_content:
+    data = json.load(file_content)
+    return data["paths"]
